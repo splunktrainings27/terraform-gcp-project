baf83681615f2a4c6711aed60a465f5749b39499
splunktrainings27 <nagendrabudipiti@gmail.com>
2026-04-06 20:02:12 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -14,3 +14,20 @@ resource "google_compute_instance" "vm" {
                  access_config {}
            }
         }
+
+resource "google_compute_instance" "vm2" {
+  name         = "test-vm-2"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
