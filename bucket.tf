edf96fdd67834fc17a62d3d134c4385b7be62035
splunktrainings27 <nagendrabudipiti@gmail.com>
2026-04-07 01:29:29 +0530

    Delete bucket.tf

--- bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "random_id" "bucket" {
-  byte_length = 4
-}
-
-resource "google_storage_bucket" "tf_bucket" {
-  name          = "terraform-state-${random_id.bucket.hex}"
-  location      = "US"
-  force_destroy = true
-}
